4e281e852d9db989d2e5266b3e42e53b38569a6c
pratikgole2004 <mr.pgole@gmail.com>
2024-11-21 00:29:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ document.getElementById('prediction-form').addEventListener('submit', async func
     event.preventDefault();
 
     const features = [];
-    for (let i = 3; i <= 22; i++) {
+    for (let i = 1; i <= 20; i++) {
         const featureValue = parseFloat(document.getElementById(`feature${i}`).value);
         features.push(featureValue);
     }
