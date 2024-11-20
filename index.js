3ec4bd4cec5b8cca3164668f0db41b1cb2acaff2
pratikgole2004 <mr.pgole@gmail.com>
2024-11-20 22:56:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ document.getElementById('prediction-form').addEventListener('submit', async func
     }
 
     try {
-        const response = await fetch('http://localhost:5000/predict', {
+        const response = await fetch(' http://127.0.0.1:5000/predict', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
